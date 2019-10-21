01d937dbd4077e452a0d01d3a162b6b3a2832bcf
Jasim Atiyeh <jasimatiyeh@gmail.com>
2019-10-21 14:35:27 -0500

    Make first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const db = require("./config/keys").mongoURI;
+
+mongoose.connect(db, { useNewUrlParser: true }).then(
+  () => console.log("Connected to mongoDB successfully!")
+).catch(
+  (err) => console.log(err)
+);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server is running on ${port}`)
+});
+
