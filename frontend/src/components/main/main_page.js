e77650f6b40c3934d1775f415fb669abde9335e3
Albert Cheng <lptalbert@gmail.com>
2019-10-22 01:59:21 -0700

    complete frontend user auth

---
+++ frontend/src/components/main/main_page.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class MainPage extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <h1>NoseyHood welcome page</h1>
+        <footer>
+          Copyright &copy; 2019 Team Members {/* change this! */}
+        </footer>
+      </div>
+    );
+  }
+}
+
+export default MainPage;
