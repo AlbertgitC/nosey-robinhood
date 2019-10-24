9abf52267b720a8dd51b53a9a69f26fb046cc299
Albert Cheng <lptalbert@gmail.com>
2019-10-24 07:42:08 -0700

    ready to deploy to heroku

--- app.js
+++ app.js
@@ -6,7 +6,14 @@ const users = require("./routes/api/users");
 const purchaseRecords = require("./routes/api/purchase_records");
 const bodyParser = require("body-parser");
 const passport = require('passport');
+const path = require('path');
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('frontend/build'));
+  app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  })
+}
 
 mongoose.connect(db, { useNewUrlParser: true })
   .then(() => console.log("Connected to mongoDB successfully!"))
