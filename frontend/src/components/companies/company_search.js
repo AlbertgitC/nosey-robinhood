72aa7ad51d48c984f6f56d4a7b002819199cbe61
Jasim Atiyeh <jasimatiyeh@gmail.com>
2019-10-25 17:39:48 -0500

    add company search feature

---
+++ frontend/src/components/companies/company_search.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import { fetchCompanySearch } from '../../actions/company_actions';
+
+class CompanySearch extends React.Component {
+
+  constructor() {
+    this.state = {
+      searchRequest: '',
+      searchResults: undefined
+    };
+
+    this.handleSubmit = this.handleSubmit.bind('this');
+    this.update = this.update.bind('this');
+    this.clearSearch = this.clearSearch.bind('this');
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    fetchCompanySearch(this.state.searchRequest)
+      .then(searchResults => this.setState({
+        searchResults: searchResults.bestMatches
+      }));
+  }
+
+  update(e) {
+    this.setState({ searchRequest: e.currentTarget.value })
+  }
+
+  clearSearch() {
+    this.setState({ searchRequest: '' });
+  }
+  
+  render() {
+    let searchResults;
+    if (this.state.searchResults) {
+      searchResults = this.state.searchResults.map((result, idx) => (
+        <li>
+          <div className='company-search-results-list-company-name'>
+            {result.name}
+          </div>
+          < div className = 'company-search-results-list-company-ticker' >
+            {result.symbol}
+          </div>
+        </li>
+      ))
+    }
+
+    return (
+      <div className='company-search'>
+        <div className='company-search-input'>
+          < i class = "material-icons" > search </i>
+          <form onSubmit={this.handleSubmit}>
+            <input
+              type='text'
+              placeholder='Search'
+              onChange={this.update}/>
+          </form>
+          < i class = "material-icons" onClick={this.clearSearch}> clear </i>
+        </div>
+        <div className='company-search-results'>
+          <ul className='company-search-results-list'>
+            {searchResults}
+          </ul>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default CompanySearch
