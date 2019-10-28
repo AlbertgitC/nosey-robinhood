484df70d228464cfcd0c277f831fdafe30a8eb9a
zach <zachcwilliams@icloud.com>
2019-10-28 10:33:22 -0700

    add navbar logout container and button

--- frontend/src/components/companies/company_navbar.js
+++ frontend/src/components/companies/company_navbar.js
@@ -1,14 +1,28 @@
 import React, { Component } from 'react'
 import CompanySearch from './company_search'
 
-class CompanyNavbar extends Component {
+class CompanyNavbar extends React.Component {
+
+  constructor(props) {
+    super(props)
+
+    this.logoutUser = this.logoutUser.bind(this);
+  }
+
+  logoutUser(e) {
+    e.preventDefault();
+    this.props.logout();
+  }
+
   render() {
+    console.log(this.props)
     return (
       <div>
-        <nav className="splash-nav-bar">
+        <nav className="nav-bar-nav-bar">
           
           <CompanySearch />
-          <span className="splash-nav-logo" id="company-nav-logo-robinhood">Nosey Robinhood</span>
+          <span id="company-nav-logo-robinhood">Nosey Robinhood</span>
+          <button onClick={this.logoutUser}>Logout</button>
         </nav>
       </div>
     )
